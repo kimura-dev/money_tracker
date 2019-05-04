364a0702ad41a6179e12157cfdbb708e809fcefc
Scott Reyell <smreyell@gmail.com>
2019-05-03 20:19:11 -0700

    created event listener on add__btn inside of controller function, addeda ctrlAddItem function as well to facilitate chagnes to data and the UI.

--- app.js
+++ app.js
@@ -2,17 +2,7 @@
 
 /*This is an immediate invoked func expression that returns an object. An IFFE allows use to have data privacy because it creates a new scope that is not visible from the outside scope. The module pattern return an obj containing all of the func that we want to be public. */
 var budgetController = (function(){
-  var x = 23;
-
-  var add = function(a){
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b){
-      return add(b);
-    }
-  }
+ 
 })();
 
 /* Seperation of concerns each part of the app should work independently not knowing the others exist. */
@@ -25,14 +15,32 @@ var UIController = (function(){
 /** Will pass the other two modules as arguments so that it connect to both. */
 var controller = (function(budgetCtrl, UICtrl){
 
-  // Show Code
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function () {
 
-  return {
-    anotherPublicFunc: function(){
-      console.log(z);
-    }
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the the UI
+
+    // 4. Calculate the budget
+
+    // 5.Display the budget to the UI
+    console.log('enter was pressed');
+  
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  // on the windows object and will give it event object
+  document.addEventListener('keypress', function (e) {
+    // console.log(e);
+
+    // which is the keyCode for some browsers
+    if(e.keyCode === 13 || e.which === 13){
+        ctrlAddItem();      
+    }
+  });
 
 })(budgetController, UIController);
 
