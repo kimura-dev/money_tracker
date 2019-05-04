eaef136e708145a7407703389ba2cc7c5295ace0
Scott Reyell <smreyell@gmail.com>
2019-05-04 10:34:59 -0700

    connected init funct

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// THE MODULE PATTERN
+/////////////////////////////////////////////
+//// THE MODULE PATTERN
+
 var budgetController = (function(){
  
 })();
@@ -34,35 +36,39 @@ var UIController = (function(){
 /** Will pass the other two modules as arguments so that it connect to both. */
 var controller = (function(budgetCtrl, UICtrl){
 
-  // var setupEventListeners = fucntion(){
+  var setupEventListeners = function(){
+    var DOM = UICtrl.getDOMstrings();
 
-  // }
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  var DOM = UICtrl.getDOMstrings();
+    document.addEventListener('keypress', function (e) {
+      // 'which' is the keyCode for some browsers
+      if(e.keyCode === 13 || e.which === 13){
+          ctrlAddItem();      
+      }
+    });
+  };
+
 
   var ctrlAddItem = function () {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
 
     // 3. Add the item to the the UI
 
     // 4. Calculate the budget
 
-    // 5.Display the budget to the UI
-    console.log('enter was pressed');
-  
-  }
+    // 5.Display the budget to the UI  
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function (e) {
-    // which is the keyCode for some browsers
-    if(e.keyCode === 13 || e.which === 13){
-        ctrlAddItem();      
+  return {
+    init: function(){
+      console.log('App Started!');
+      setupEventListeners();
     }
-  });
+  };
 
 })(budgetController, UIController);
 
+controller.init(); // Only line of code placed outside of my controllers
