42b8246c4442d215bc485af0a4680ab5e0e19da9
Scott Reyell <smreyell@gmail.com>
2019-05-06 16:49:42 -0700

    added conditional inside cntrlAddItem to make all inputs correct values before running the function

--- app.js
+++ app.js
@@ -80,7 +80,8 @@ var UIController = (function(){
       return {
         type: document.querySelector(DOMstrings.inputType).value, 
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        // parseFloat converts a string into a number with decimals
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -149,21 +150,31 @@ var controller = (function(budgetCtrl, UICtrl){
     });
   };
 
+  var updateBudget = function(){
+     // 1. Calculate the budget
+
+     //  2 return the budget
+
+    // 3.Display the budget to the UI  
+  };
 
   // Control center of the application
   var ctrlAddItem = function () {
     var input, newItem;
     // 1. Get the field input data
     input = UICtrl.getInput();
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    // 3. Add the item to the the UI
-    UICtrl.addListItem(newItem, input.type);
-    // 4. Clear the fields
-    UICtrl.clearFields();
-    // 5. Calculate the budget
 
-    // 5.Display the budget to the UI  
+    if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. Add the item to the the UI
+      UICtrl.addListItem(newItem, input.type);
+      // 4. Clear the fields
+      UICtrl.clearFields();
+      // 5. Calculate and update budget
+      updateBudget();
+    }
+   
   };
 
   return {
