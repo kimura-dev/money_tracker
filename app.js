b2e27b46f1541343f52eb57500845f4fa40d1f44
Scott Reyell <smreyell@gmail.com>
2019-05-06 18:06:26 -0700

    added budget, inc, exp, and percentage labels to DOMstrings. I then added them into an new public func, displayBudget which manipulates the DOM and is displaying the values properly.

--- app.js
+++ app.js
@@ -108,12 +108,16 @@ var budgetController = (function(){
 var UIController = (function(){
 
   var DOMstrings = {
-    inputType: '.add__type',
-    inputDescription: '.add__description',
-    inputValue: '.add__value',
-    inputBtn: '.add__btn',
-    incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn',
+      incomeContainer: '.income__list',
+      expensesContainer: '.expenses__list',
+      budgetLabel: '.budget__value',
+      incomeLabel: '.budget__income--value',
+      expensesLabel: '.budget__expenses--value',
+      percentageLabel: '.budget__expenses--percentage'
   };
 
   // This is a public function 
@@ -167,6 +171,16 @@ var UIController = (function(){
 
     },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+    },
+
     // Makes DOM string variables public
     getDOMstrings: function() {
       return DOMstrings;
@@ -200,7 +214,7 @@ var controller = (function(budgetCtrl, UICtrl){
      //  2 return the budget
     var budget = budgetCtrl.getBudget();
     // 3.Display the budget to the UI  
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   // Control center of the application
